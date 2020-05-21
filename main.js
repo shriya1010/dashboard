a286c1186142646448b1c36acb3119a9dd1a8a02
Shriya <shriyasawant89@gmail.com>
2020-05-21 17:56:44 +0530

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var a=axios.get('http://localhost:3000/client') .then(function(response){ 
     var txt="";
 info=response.data
+txt+="<div class='row'>"
 for (x in info) 
 {
 txt += "<div class='box' ><img src='"+info[x].image+"' alt='image failed to load'><p>" + info[x].name + "</p>"
@@ -8,6 +9,7 @@ txt +="<a href='"+info[x].email+"'>"+info[x].email+"</a><div class='button'>"
 txt +="<button type='button' class='btn btn-outline-dark'><i class='fa fa-user-o'></i>Profile</button>&nbsp;"
 txt+="<button type='button' class='btn btn-outline-dark'><i class='fa fa-envelope-o'></i>Contact</button></div></div>"
   }
+  txt+="</div>"
       
  document.getElementById("display").innerHTML = txt; 
 

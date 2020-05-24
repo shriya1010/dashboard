f9a0b00b11ffb7acddb4e80353c408ca6d8d249d
Shriya <shriyasawant89@gmail.com>
2020-05-24 23:47:03 +0530

    commit

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-var a=axios.get('http://localhost:3000/client') .then(function(response){ 
-    var data="";
-info=response.data
+axios.get('http://localhost:3000/client') .then(function(response){ 
+    
+var info=response.data
+var data="",index,i;
 for (index in info) 
 {
 data += "<div class='box' ><img src='"+info[index].image+"' alt='image failed to load'><p>" + info[index].name + "</p>"
-data +="<a href='"+info[index].email+"'>"+info[index].email+"</a>"
-data +="<div class='button'><button type='button' ><i class='fa fa-user-o'></i>&nbsp;&nbsp;Profile</button>"
+data +="<a href='"+info[index].email+"'>"+info[index].email+"</a><div>"
+
+for(i=0;i<(info[index].contact).length;i++)
+{
+  data += "<img src='"+info[index].contact[i]+"' alt='image failed to load'>"
+}
+
+data +="</div><div class='button'><button type='button' ><i class='fa fa-user-o'></i>&nbsp;&nbsp;Profile</button>"
 data+="<button type='button' ><i class='fa fa-envelope-o'></i>&nbsp;&nbsp;Contact</button></div></div>"
   }
       
